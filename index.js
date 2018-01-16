88f0f01ccf047ee2b07548bd9cdf9399521e5d1c
Jordi Baylina <jordi@baylina.cat>
2018-01-16 13:18:58 +0100

    Fix doble

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const httpProxy = require('http-proxy');
 
 const proxy = httpProxy.createProxyServer({});
 
-
 http.createServer(async (req, res) => {
-    console.log("url: ", req.url);
-    console.log("hosnname: ", req.headers.host);
     const content = await ens.getContent(req.headers.host);
-    console.log("content: ", content);
-  // This simulates an operation that takes 500ms to execute
+    const url = 'http://localhost:9001' + content;
+    console.log(url);
     proxy.web(req, res, {
-      target: 'http://localhost:9001' + content + req.url
+      target: url
     });
 }).listen(80);
